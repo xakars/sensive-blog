bdcd736279910918669ff1f59b7f5208ab861c7d
Arsen <xakars02@gmail.com>
2022-10-23 13:38:37 +0600

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -48,11 +48,12 @@ def serialize_post_optimized(post):
 
 def index(request):
     most_popular_posts = Post.objects.popular() \
-                             .prefetch_related('author')[:5] \
-                             .fetch_with_comments_count()
+                             .prefetch_related('author', 'tags')[:5] \
+                             .fetch_with_comments_count() \
 
-
-    fresh_posts = Post.objects.annotate(comments_count=Count('comments')).order_by('published_at').prefetch_related('author')
+    fresh_posts = Post.objects.annotate(comments_count=Count('comments'))\
+                              .order_by('published_at')\
+                              .prefetch_related('author', 'tags')
     most_fresh_posts = list(fresh_posts)[-5:]
 
     most_popular_tags = Tag.objects.popular()[:5]
