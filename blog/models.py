1ceb2a8955190e573dc92b4958dd3205f5174db6
Arsen <xakars02@gmail.com>
2022-10-28 15:42:41 +0600

    fix manager position in post model

--- blog/models.py
+++ blog/models.py
@@ -36,7 +36,6 @@ class Post(models.Model):
     slug = models.SlugField('Название в виде url', max_length=200)
     image = models.ImageField('Картинка')
     published_at = models.DateTimeField('Дата и время публикации')
-    objects = PostQuerySet.as_manager()
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -51,6 +50,7 @@ class Post(models.Model):
         'Tag',
         related_name='posts',
         verbose_name='Теги')
+    objects = PostQuerySet.as_manager()
 
     def __str__(self):
         return self.title
